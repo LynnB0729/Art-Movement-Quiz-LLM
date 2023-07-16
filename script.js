8dcc1cfbb946cf6eca7c27dec4a01e222f950463
Lynn <simplylynnhandmade@gmail.com>
2023-07-16 19:58:47 -0400

    Adding Books
    adding books & links

--- script.js
+++ script.js
@@ -156,63 +156,63 @@ var fashionItems = {
 var bookSuggestions = {
     "Ancient Art (Egyptian, Greek, Roman, etc.)": {
         "book": "The Egyptian Book of the Dead",
-        "image": "https://example.com/images/egyptian_book.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/91YvW5cIRlL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Egyptian-Book-Dead-Integrated-Full-Color/dp/1452144389/ref=sr_1_1?keywords=The+Egyptian+Book+of+the+Dead&qid=1689550900&sr=8-1&tag=alproviste07-20"
     },
     "Gothic Art": {
         "book": "The Divine Comedy by Dante Alighieri",
-        "image": "https://example.com/images/divine_comedy.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/910RVMMAJpL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Divine-Comedy-Inferno-Purgatorio-Paradiso/dp/0451208633/ref=sr_1_1?keywords=The+Divine+Comedy+by+Dante+Alighieri&qid=1689550923&sr=8-1&tag=alproviste07-20"
     },
     "Renaissance Art": {
         "book": "The Birth of Venus by Sarah Dunant",
-        "image": "https://example.com/images/birth_of_venus.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/913CkKNvWoL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Birth-Venus-Novel-Readers-Circle/dp/0812968972/ref=sr_1_1?keywords=The+Birth+of+Venus+by+Sarah+Dunant&qid=1689550951&sr=8-1&tag=alproviste07-20"
     },
     "Baroque Art": {
         "book": "The Girl with the Pearl Earring by Tracy Chevalier",
-        "image": "https://example.com/images/girl_with_pearl_earring.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/91Xg3PAEfuL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Girl-Pearl-Earring-Tracy-Chevalier/dp/0452282152/ref=sr_1_1?keywords=The+Girl+with+the+Pearl+Earring+by+Tracy+Chevalier&qid=1689550974&sr=8-1&tag=alproviste07-20"
     },
     "Romanticism": {
         "book": "Frankenstein by Mary Shelley",
-        "image": "https://example.com/images/frankenstein.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/81z7E0uWdtL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Frankenstein-Mary-Shelley/dp/1512308056/ref=sr_1_1?keywords=Frankenstein+by+Mary+Shelley&qid=1689550994&sr=8-1&tag=alproviste07-20"
     },
     "Impressionism": {
         "book": "Claude & Camille by Stephanie Cowell",
-        "image": "https://example.com/images/claude_and_camille.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/61CUkSl49zL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/sspa/click?ie=UTF8&spc=MTozNTg0NTI2MzUwODM5Mjk1OjE2ODk1NTEwMTI6c3BfYXRmOjIwMDE3Mjg4NTUyNjU5ODo6MDo6&url=%2FAstro-School-Project-Claude-Jones%2Fdp%2F1733843949%2Fref%3Dsr_1_1_sspa%3Fkeywords%3DClaude%26qid%3D1689551012%26sr%3D8-1-spons%26sp_csd%3Dd2lkZ2V0TmFtZT1zcF9hdGY%26psc%3D1&tag=alproviste07-20"
     },
     "Post-Impressionism": {
         "book": "The Yellow House: Van Gogh, Gauguin, and Nine Turbulent Weeks in Arles by Martin Gayford",
-        "image": "https://example.com/images/yellow_house.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/81nYqxcVIzL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Yellow-House-Gauguin-Turbulent-Provence/dp/0618990585/ref=sr_1_1?keywords=The+Yellow+House%3A+Van+Gogh%2C+Gauguin%2C+and+Nine+Turbulent+Weeks+in+Arles+by+Martin+Gayford&qid=1689551042&sr=8-1&tag=alproviste07-20"
     },
     "Cubism": {
         "book": "The Cubist Epoch by Douglas Cooper",
-        "image": "https://example.com/images/cubist_epoch.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/71pMHlbDTpL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Cubist-Epoch-Douglas-Cooper/dp/0875870414/ref=sr_1_1?keywords=The+Cubist+Epoch+by+Douglas+Cooper&qid=1689551067&sr=8-1&tag=alproviste07-20"
     },
     "Surrealism": {
         "book": "Nadja by Andre Breton",
-        "image": "https://example.com/images/nadja.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/41Ut+n1lzXL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Nadja-Andr%C3%A9-Breton/dp/0802150268/ref=sr_1_1?keywords=Nadja+by+Andre+Breton&qid=1689551088&sr=8-1&tag=alproviste07-20"
     },
     "Abstract Expressionism": {
         "book": "De Kooning: An American Master by Mark Stevens and Annalyn Swan",
-        "image": "https://example.com/images/de_kooning.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/916ohXhp0SL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Kooning-American-Master-Mark-Stevens/dp/0375711163/ref=sr_1_2?keywords=De+Kooning%3A+An+American+Master+by+Mark+Stevens+and+Annalyn+Swan&qid=1689551115&sr=8-2&tag=alproviste07-20"
     },
     "Pop Art": {
         "book": "Pop Art: A Critical History by Steven Henry Madoff",
-        "image": "https://example.com/images/pop_art.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/61Z3K9Y1qiL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Pop-Art-Critical-Documents-Twentieth-Century/dp/0520212436/ref=sr_1_1?keywords=Pop+Art%3A+A+Critical+History+by+Steven+Henry+Madoff&qid=1689551145&sr=8-1&tag=alproviste07-20"
     },
     "Contemporary Art": {
         "book": "Seven Days in the Art World by Sarah Thornton",
-        "image": "https://example.com/images/seven_days.jpg",
-        "link": "https://www.amazon.com/dp/B07H9HJLG5?tag=your_affiliate_id"
+        "image": "https://m.media-amazon.com/images/I/61TgVEBVUlL._AC_UY218_.jpg",
+        "link": "https://www.amazon.com/Seven-Days-World-Sarah-Thornton/dp/039333712X/ref=sr_1_1?keywords=Seven+Days+in+the+Art+World+by+Sarah+Thornton&qid=1689551196&sr=8-1&tag=alproviste07-20"
     }
 };
 
